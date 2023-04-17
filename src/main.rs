2b6ab72aceb2b63fbf4f4fe741b25ceaf86fcc1a
Erik Magnusson <erik.arn.magnusson@gmail.com>
2023-04-18 01:02:09 +0200

    CLI

--- src/main.rs
+++ src/main.rs
@@ -9,96 +9,74 @@
 //use pascal_parser_rust::State;
 use pascal_parser_rust::ProgParser;
 use pascal_parser_rust::State;
+use std::env::args;
 use std::time::Instant;
-fn main() {
-    let _p0 = "
-program testok1(input, output);  
-var A, B, C: integer;  
+fn main() -> Result<(), ()> {
+    let execute = args().find(|s| s == "-x").is_some();
+    let compile = args().find(|s| s == "-c").is_some();
+    let help = args().find(|s| s == "-h" || s == "--help").is_some();
 
-begin
-    A := B + C * 2;
-    B := A + integer[B * 2 + C];
-    real[3] := 3.4;
-    C := 5;
-    while C > 2 do
-    begin
-        C := C - 1
-    end
-end.
-";
-    let _fib = "
-program fib(input, output);  
-var A, B, C: integer;  
+    if help {
+        eprintln!("Usage (any order):");
+        eprintln!("   cargo run -- [filename] [-x] [-c] [-h] [--help]");
+        eprintln!();
+        eprintln!("   filename: input filename (can also read from stdin)");
+        eprintln!("   -x: execute program");
+        eprintln!("   -c: compile program to rust code");
+        eprintln!("   -h or --help: print this help text");
+        eprintln!();
+        eprintln!("Examples:");
+        eprintln!("    cargo run -- example_programs/prim.pas -x");
+        eprintln!("    cargo run -- example_programs/fib.pas -c");
+        eprintln!("    cat example_programs/fib.pas | cargo run -- -x");
+        return Ok(());
+    }
 
-begin
-    A := 0;
-    B := 1;
-    C := 1;
-    while C < 1000 do
-    begin
-        C := A + B;
-        A := B + C;
-        B := C + A
-    end
-end.
-";
-    let prim = "
-program prim(input, output);  
-var i, j, maxNums: integer;  
-
-begin
-    maxNums := 10000;
-    i := 2;
-    while i < maxNums do
-    begin
-        while i < maxNums && boolean[i] do
-        begin
-            i := i + 1
-        end;
-        if i < maxNums then
-        begin
-            writeLn i;
-            boolean[i] := true;
-            j := 0;
-            while j <= maxNums do
-            begin
-                boolean[j] := true;
-                j := j + i
-            end
-        end
-    end
-end.
-";
-
-    let program = prim;
-
-    let mut state = State::default();
-
-    let execute = true;
-    let compile = true;
-
-    eprintln!("Program:\n{program}\n$");
-    match ProgParser::new().parse(&mut state, program) {
-        Err(e) => {
-            eprintln!("{:?}", &state.variable_table);
-            eprintln!("{e:?}");
+    // monads are fun
+    match args()
+        .map(std::fs::read_to_string)
+        .map(Result::ok)
+        .flatten()
+        .next()
+        .or_else(|| {
+            eprintln!("Reading program from stdin... (Ctrl-D to send EOF)");
+            std::io::read_to_string(std::io::stdin()).ok()
+        }) {
+        None => {
+            eprintln!("Error reading from file and standard input");
+            Err(())
         }
-        Ok(parsed) => {
-            eprintln!("{:?}", &state.variable_table);
-            eprintln!("{:?}", &parsed);
-            if execute {
-                eprintln!("executing...");
-                let start = Instant::now();
-                parsed.execute(&mut state);
-                let duration = start.elapsed();
+        Some(program) => {
+            let mut state = State::default();
 
-                eprintln!("done...");
-                eprintln!("{:?}", &state.variable_table);
-                eprintln!("Execution completed in {duration:?}");
-            }
-            if compile {
-                eprintln!("compiling...");
-                println!("{}", parsed.compile(&state));
+            eprintln!("Program:\n{program}\n$");
+            match ProgParser::new().parse(&mut state, &program) {
+                Err(e) => {
+                    eprintln!("COULD NOT PARSE!");
+                    eprintln!("{:?}", &state.variable_table);
+                    eprintln!("{e:?}");
+                    Err(())
+                }
+                Ok(parsed) => {
+                    eprintln!("PARSE OK!");
+                    eprintln!("{:?}", &state.variable_table);
+                    eprintln!("{:?}", &parsed);
+                    if execute {
+                        eprintln!("executing...");
+                        let start = Instant::now();
+                        parsed.execute(&mut state);
+                        let duration = start.elapsed();
+
+                        eprintln!("done...");
+                        eprintln!("{:?}", &state.variable_table);
+                        eprintln!("Execution completed in {duration:?}");
+                    }
+                    if compile {
+                        eprintln!("compiling...");
+                        println!("{}", parsed.compile(&state));
+                    }
+                    Ok(())
+                }
             }
         }
     }
